3a79b7658faa0ea13b76b09e2fd3d76efc599139
Kristian1505 <160262077+Kristian1505@users.noreply.github.com>
2024-03-10 20:32:18 -0600

    Create scripts.js

---
+++ scripts.js
@@ -0,0 +1,19 @@
+// Exercise #1 (sugerencia: join())
+
+// Copy the code below into your workspace:
+
+let arr = ["This", "is", "a", "sentence."];
+
+function printOutString() {
+    for (let i = 0; i < arr.length; i++) {
+        console.log(arr.join(" "))
+        
+    }
+
+  // your code here
+
+}
+
+
+printOutString();
+
